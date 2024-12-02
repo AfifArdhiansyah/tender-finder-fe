616fb4924d25f5dfed91c5eea36156659b7cd972
AfifArdhiansyah <afifardhiansyah@gmail.com>
2024-12-02 17:39:00 +0700

    feat: relation tender from be

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -43,33 +43,49 @@ export default function User(){
     };
     return(
         <DashboardLayout sideNavIndex={10} bcItems={bcItems}>
-            <Paper className="flex flex-col gap-6 pb-6">
-                <div className="flex flex-col gap-1">
-                    <h3 className="font-bold text-lg">Informasi Pegawai</h3>
-                    <p className="text-sm text-gray-500">Informasi dasar pegawai, jika ada kesalahan data anda dapat menghubungi tim manajemen</p>
-                </div>
-                {
-                    loading ? (
-                        <Loading/>
-                    ) : error ? (
-                        <Response type="error" message={error}/>
-                    ) : (
-                        <div className="grid grid-cols-12 gap-2 max-md:text-sm">
-                            <p className="col-span-4 md:col-span-2">Foto Profile</p>
-                            <Image src={user?.role=="ao"?"/manager2.png":"/manager.png"} width={120} height={120} className='rounded col-span-8 md:col-span-10' alt='foto-profile' />
-                            <p className="col-span-4 md:col-span-2">Nama</p>
-                            <p className="col-span-8 md:col-span-10">: <span className="text-gray-500">{user?.nama}</span></p>
-                            <p className="col-span-4 md:col-span-2">NIP</p>
-                            <p className="col-span-8 md:col-span-10">: <span className="text-gray-500">{user?.nip}</span></p>
-                            <p className="col-span-4 md:col-span-2">Posisi</p>
-                            <p className="col-span-8 md:col-span-10">: <span className="text-gray-500">{getRole(user?.role as string)}</span></p>
-                            <p className="col-span-4 md:col-span-2">Lokasi</p>
-                            <p className="col-span-8 md:col-span-10">: <span className="text-gray-500">{getKantor(user?.role as string)}</span></p>
-                        </div>
-                    )
-                }
-                <Button type={"danger"} size={"medium"} onClick={handleLogout} className="w-fit">logout</Button>
-            </Paper>
+            {
+                loading ? (
+                    <Loading/>
+                ) : error ? (
+                    <Response type="error" message={error}/>
+                ) : (
+                    <div className="flex flex-col gap-4">
+                        <Paper className="flex flex-col gap-6 pb-6">
+                            <div className="flex flex-col gap-1">
+                                <h3 className="font-bold text-lg">Informasi Pegawai</h3>
+                                <p className="text-sm text-gray-500">Informasi dasar pegawai, jika ada kesalahan data anda dapat menghubungi tim manajemen</p>
+                            </div>
+                            <div className="grid grid-cols-12 gap-2 max-md:text-sm">
+                                <p className="col-span-4 md:col-span-2">Foto Profile</p>
+                                <Image src={user?.role=="ao"?"/manager2.png":"/manager.png"} width={120} height={120} className='rounded col-span-8 md:col-span-10' alt='foto-profile' />
+                                <p className="col-span-4 md:col-span-2">Nama</p>
+                                <p className="col-span-8 md:col-span-10">: <span className="text-gray-500">{user?.nama}</span></p>
+                                <p className="col-span-4 md:col-span-2">NIP</p>
+                                <p className="col-span-8 md:col-span-10">: <span className="text-gray-500">{user?.nip}</span></p>
+                                <p className="col-span-4 md:col-span-2">Posisi</p>
+                                <p className="col-span-8 md:col-span-10">: <span className="text-gray-500">{getRole(user?.role as string)}</span></p>
+                                <p className="col-span-4 md:col-span-2">Lokasi</p>
+                                <p className="col-span-8 md:col-span-10">: <span className="text-gray-500">{getKantor(user?.role as string)}</span></p>
+                            </div>
+                            <Button type={"danger"} size={"medium"} onClick={handleLogout} className="w-fit">logout</Button>
+                        </Paper>
+                        <Paper className="flex flex-col gap-6 pb-6">
+                            <div className="flex flex-col gap-1">
+                                <h3 className="font-bold text-lg">Informasi Kantor</h3>
+                                <p className="text-sm text-gray-500">Informasi kantor pegawai</p>
+                            </div>
+                            <div className="grid grid-cols-12 gap-2 max-md:text-sm">
+                                <p className="col-span-4 md:col-span-2">Nama</p>
+                                <p className="col-span-8 md:col-span-10">: <span className="text-gray-500">{user?.office.nama}</span></p>
+                                <p className="col-span-4 md:col-span-2">Alamat</p>
+                                <p className="col-span-8 md:col-span-10">: <span className="text-gray-500">{user?.office.alamat}</span></p>
+                                <p className="col-span-4 md:col-span-2">Tipe</p>
+                                <p className="col-span-8 md:col-span-10">: <span className="text-gray-500">{user?.office.type}</span></p>
+                            </div>
+                        </Paper>
+                    </div>
+                )
+            }
         </DashboardLayout>
     )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,32 +7,14 @@ interface NavbarProps{
 }
 
 export default function Navbar(props: NavbarProps){
-    const {role} = useUser()
-    const [currRole, setCurrRole] = useState(role)
-    useEffect(() => {
-        setCurrRole(role)
-    }, [role])
-    function getTitle(){
-        switch(currRole){
-            case 'manager-pusat':
-                return 'Kantor Pusat'
-            case 'manager-kanwil':
-                return 'Kantor Wilayah'
-            case 'manager-cabang':
-                return 'Kantor Cabang'
-            case 'ao':
-                return 'Account Officer'
-            default:
-                return 'Kantor Pusat'
-        }
-    }
+    const {officeName} = useUser()
     return(
         <div className="bg-white flex justify-between items-center py-6 px-6 max-md:px-4">
             <div className="max-md:flex max-md:gap-4 max-md:items-center">
                 <button className="md:hidden flex justify-between items-center" onClick={props.toggleSidebar()} >
                     <HiMenu size={30}/>
                 </button>
-                <h2 className="font-bold text-lg max-md:text-sm">{getTitle()}</h2>
+                <h2 className="font-bold text-lg max-md:text-sm">{officeName}</h2>
             </div>
             <div className="flex flex-col text-grey max-md:text-sm">
                 <p>Data Terbaru:</p>
