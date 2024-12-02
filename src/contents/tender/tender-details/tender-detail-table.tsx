39b240c93edc86bdbdd16ae435f70b387c2b24b6
AfifArdhiansyah <afifardhiansyah@gmail.com>
2024-12-02 23:59:08 +0700

    feat: implement tender statuses progress

--- src/contents/tender/tender-details/tender-detail-table.tsx
+++ src/contents/tender/tender-details/tender-detail-table.tsx
@@ -1,54 +1,43 @@
-import { useGetTenderById } from "@/hooks/useTenderProjects"
-import Loading from "@/components/items/progress/loading";
-import Response from "@/components/items/responses/response";
+import { TenderProjectModel } from "@/models/tender-project-model";
 
 interface TenderDetailTableProps{
     idTender: string
+    tenderProject?: TenderProjectModel
 }
 
 export default function TenderDetailTable(props: TenderDetailTableProps){
-    const { tenderProject, loading, error } = useGetTenderById(props.idTender);
+    
     return(
-        <>
-            {
-                loading ? (
-                    <Loading/>
-                ) : error ? (
-                   <Response type="error" message={error}/>
-                ) : (
-                    <div className="grid grid-cols-12 gap-y-2 text-sm">
-                        <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Nomor Tender</p>
-                        <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> {tenderProject?.id}</p>
+        <div className="grid grid-cols-12 gap-y-2 text-sm">
+            <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Nomor Tender</p>
+            <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> {props.tenderProject?.id}</p>
 
-                        <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Nama Tender</p>
-                        <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> {tenderProject?.nama}</p>
+            <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Nama Tender</p>
+            <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> {props.tenderProject?.nama}</p>
 
-                        <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Nama Pemenang</p>
-                        <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> {tenderProject?.nama_pemenang}</p>
+            <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Nama Pemenang</p>
+            <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> {props.tenderProject?.nama_pemenang}</p>
 
-                        <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Nominal Tender</p>
-                        <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> Rp. {parseFloat(tenderProject?.nilai_tender as string).toLocaleString('id-ID')}</p>
+            <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Nominal Tender</p>
+            <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> Rp. {parseFloat(props.tenderProject?.nilai_tender as string).toLocaleString('id-ID')}</p>
 
-                        <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Alamat Tender</p>
-                        <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> {tenderProject?.lokasi_pekerjaan}</p>
+            <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Alamat Tender</p>
+            <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> {props.tenderProject?.lokasi_pekerjaan}</p>
 
-                        <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Nama AO</p>
-                        <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> {tenderProject?.account_officer.nama}</p>
+            <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Nama AO</p>
+            <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> {props.tenderProject?.account_officer.nama}</p>
 
-                        <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Dokumen Tanda Terima</p>
-                        <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> ini dokumen tanda terima</p>
+            <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Dokumen Tanda Terima</p>
+            <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> ini dokumen tanda terima</p>
 
-                        <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Foto Follow Up</p>
-                        <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> ini data follow up</p>
+            <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Foto Follow Up</p>
+            <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> ini data follow up</p>
 
-                        <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Status Penerimaan</p>
-                        <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> {tenderProject?.tender_statuses[tenderProject.tender_statuses?.length-1].status.nama}</p>
+            <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Status Penerimaan</p>
+            <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> {props.tenderProject?.tender_statuses[props.tenderProject.tender_statuses?.length-1].status.nama}</p>
 
-                        <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Produk Yang Dipilih</p>
-                        <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> {"KMKK"}</p>
-                    </div>
-                )
-            }
-        </>
+            <p className="font-bold col-span-2 max-md:col-span-5 max-lg:col-span-4">Produk Yang Dipilih</p>
+            <p className="text-gray-500 col-span-10 max-md:col-span-7 max-lg:col-span-8"><span className="font-bold mr-2">:</span> {"KMKK"}</p>
+        </div>
     )
 }
--- src/contents/tender/tender-details/tender-detail.tsx
+++ src/contents/tender/tender-details/tender-detail.tsx
@@ -1,19 +1,33 @@
 import TenderDetailTable from "./tender-detail-table"
 import ProgressTrack from "./progress-track/progress-track"
-import { TenderTrackDumpData } from "@/constants/tender/tender-track-data"
+import { useGetTenderById } from "@/hooks/useTenderProjects"
+import Loading from "@/components/items/progress/loading";
+import Response from "@/components/items/responses/response";
+import { TenderStatusModel } from "@/models/tender-status-model";
 
 interface TenderDetailContentProps{
     idTender: string
 }
 
 export default function TenderDetailContent(props: TenderDetailContentProps){
+    const { tenderProject, loading, error } = useGetTenderById(props.idTender);
     return(
-        <div className="flex flex-col gap-4 pb-6">
-            <h2 className="font-bold text-xl">Progres Tender</h2>
-            <TenderDetailTable idTender={props.idTender}/>
-            <hr />
-            <h2 className="font-bold text-xl">Tracking</h2>
-            <ProgressTrack datas={TenderTrackDumpData}/>
-        </div>
+        <>
+            {
+                loading ? (
+                    <Loading />
+                ) : error ? (
+                    <Response message={error} type={"error"} />
+                ) : (
+                    <div className="flex flex-col gap-4 pb-6">
+                        <h2 className="font-bold text-xl">Progres Tender</h2>
+                        <TenderDetailTable tenderProject={tenderProject} idTender={props.idTender}/>
+                        <hr />
+                        <h2 className="font-bold text-xl">Tracking</h2>
+                        <ProgressTrack datas={tenderProject?.tender_statuses as TenderStatusModel[]} nilaiTender={tenderProject?.nilai_tender as string}/>
+                    </div>
+                )
+            }
+        </>
     )
 }
