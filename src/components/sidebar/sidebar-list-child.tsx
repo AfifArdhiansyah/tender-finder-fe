42f3cfbd0144cd4fe80e549e10a011921fc742a7
AfifArdhiansyah <afifardhiansyah@gmail.com>
2024-12-30 09:45:45 +0700

    feat: implement unread count

--- src/components/sidebar/sidebar-list-child.tsx
+++ src/components/sidebar/sidebar-list-child.tsx
@@ -1,6 +1,7 @@
 import React, {ReactNode } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useUnreadMessage } from "@/hooks/useMessage";
 
 interface SidebarListChildProps{
     isActive: boolean,
@@ -13,6 +14,7 @@ interface SidebarListChildProps{
 }
 
 export default function SidebarListChild(props: SidebarListChildProps){
+    const { unreadCount, loading, error } = useUnreadMessage()
     function switchActiveClass(isActive: boolean):string{
         const activeClass = ""
         switch(isActive){
@@ -22,6 +24,15 @@ export default function SidebarListChild(props: SidebarListChildProps){
                 return activeClass + "text-gray-600"
         }
     }
+    function switchCountActiveClass(isActive: boolean):string{
+        const activeClass = ""
+        switch(isActive){
+            case true:
+                return activeClass + "bg-white text-blue-500"
+            case false:
+                return activeClass + "bg-blue-500 text-white"
+        }
+    }
     function onListClicked(){
         props.onClickChild(props.navIndex)
     }
@@ -29,6 +40,11 @@ export default function SidebarListChild(props: SidebarListChildProps){
         <Link href={props.ref} className={"w-full px-4 py-2 flex gap-4 items-center rounded-[10px] md:justify-center lg:justify-start "+ switchActiveClass(props.isActive)} onClick={onListClicked}>
             <Image src={props.isActive? props.iconPath+"-active.png" : props.iconPath+".png"} alt="icon" width={20} height={20}/>
             <p className="md:hidden lg:block">{props.label}</p>
+            {
+                (props.label == "Notifikasi" && (unreadCount!=null && unreadCount>0)) && (
+                    <div className={"rounded-full h-[18px] w-[18px] flex items-center justify-center text-xs " + switchCountActiveClass(props.isActive)}>{unreadCount}</div>
+                )
+            }
         </Link>
     )
 }
